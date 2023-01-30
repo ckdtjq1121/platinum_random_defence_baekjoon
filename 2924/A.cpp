64a1d1273cac9b5c28f6428be9f2045d226345e0
ckdtjq1121 <80ckdtjq@naver.com>
2023-01-30 12:04:56 +0900

    init

---
+++ 2924/A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define int int64_t
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vvvi vector<vector<vector<int>>>
+
+#define pii pair<int, int>
+#define vp vector<pii>
+#define vvp vector<vp>
+
+#define vc vector<char>
+#define vvc vector<vc>
+
+#define vd vector<double>
+#define vvd vector<vd>
+
+using namespace std;
+
+const int MOD = 1e9 + 7;
+const int INF = INT_MAX;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    
+}
